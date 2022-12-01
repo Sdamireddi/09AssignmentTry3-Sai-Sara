36217b436e7eda4189268629bad19b8f1b5bef4c
Sdamireddi <sdamireddi@wustl.edu>
2022-12-01 14:44:50 -0600

    SetUp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
+import NewTodo from './NewTodo';
+import Todo from './Todo';
 
 class App extends Component {
+
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     todos: []
+  //   }
+  //   // set variables here
+  //   // bind event handlers/methods here
+  // }
+
+  //didMount
+  //addItem
+  //removeItem
+  //onChange
+
+
   render() {
     return (
-      <div className="App">
-        <h1>ToDo YesApp</h1>
+      <div>
+        <div className="headerstuff">
+          <h1 className="center" id="title">A ToDo App</h1>
+          <h2 className="center" id="author">By Sara Lichtarge and Sai Damireddi</h2>
+        </div>
+
+        <div className="top">
+          <h3> Your To-Do List</h3>
+        </div>
+        <hr></hr>
+
+          <div id="container">
+            <ul id="listOfItems">
+              <Todo />
+            </ul>
+          </div>
+
+        <NewTodo />
+
       </div>
+
     );
   }
 }
