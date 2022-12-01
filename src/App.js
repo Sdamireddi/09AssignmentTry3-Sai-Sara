b48d6ac29324394d3edb6f2f3869520dcf599d48
slichtarge <slichtarge@wustl.edu>
2022-12-01 17:39:06 -0600

    setup complete
    delete runs automatically as soon as the page is loaded --> why

--- src/App.js
+++ src/App.js
@@ -3,7 +3,62 @@ import './App.css';
 import Todo from './Todo';
 import NewTodo from './NewTodo';
 
+const url = "https://cse204.work/todos";
+const api_key = "69ed88-c749fc-d73c52-0c5e80-ee5441";
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {todos : []}
+  }
+
+  componentDidMount(){
+    const self = this;
+    //AJAX
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let todo_array = JSON.parse(this.responseText);
+            console.log("connection established!!!");
+            self.setState({todos : todo_array});
+            for (var x=0; x<todo_array.length; x++){
+                let todo_item = todo_array[x];
+                console.log(todo_item);
+                // display(todo_item);
+            }
+        }
+    };
+
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("x-api-key", api_key);
+    xhttp.send();
+  }
+
+  deleteItem(id){
+    console.log("you wanna delete something.")
+    // const self = this;
+    // let xhttp4 = new XMLHttpRequest();
+    // xhttp4.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         self.setState({todos: 
+    //           self.state.todos.filter(function(todo){
+    //             return todo !== id.target.value;
+    //           })
+    //         });
+    //         // let list = document.getElementById("listOfItems");
+    //         // let item = document.getElementById(id);
+    //         // list.removeChild(item);
+    //     }
+    // };
+
+    // xhttp4.open("DELETE", url+"/"+id, true);
+    // xhttp4.setRequestHeader("Content-type", "application/json");
+    // xhttp4.setRequestHeader("x-api-key", api_key);
+    // xhttp4.send();
+  }
+
   render() {
     return (
       <div>
@@ -19,7 +74,15 @@ class App extends Component {
 
         <div id="container">
           <ul id="listOfItems">
-            <Todo />
+          {this.state.todos.map((todo)=>
+            <Todo id = {todo.id}
+            text = {todo.text}
+            key = {todo.id}
+            completed = {todo.completed}
+            created_at = {todo.created_at}
+            deleteItem = {this.deleteItem}
+            />
+          )}
           </ul>
         </div>
 
--- src/Todo.js
+++ src/Todo.js
@@ -5,9 +5,9 @@ class Todo extends Component {
   render() {
     return (
         <li className="todoItem">
-        <input className="completeBox" type="checkbox"></input>
-        <p>{this.props.value}Test</p>
-        <input className="deleteButton" type="button" name="deleteButton" value="X"></input>
+        <input className="completeBox" type="checkbox" /*onClick = {this.props.updateItem}*/></input>
+        <p>{this.props.text}</p>
+        <input className="deleteButton" type="button" name="deleteButton" value="X" onClick = {this.props.deleteItem(this.props.id)}></input>
       </li>
     );
   }
